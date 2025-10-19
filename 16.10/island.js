361df38651d635f6786f6bdca66cfeef7b199728
karapetyanani415-dot <karapetyanani415@gmail.com>
2025-10-19 23:29:15 +0400

    homework

--- 16.10/island.js
+++ 16.10/island.js
@@ -3,17 +3,16 @@ function islandPerimeter(grid) {
     for (let i = 0; i < grid.length; i++) {
         for (let j = 0; j < grid[i].length; j++) {
             if (grid[i][j] === 1) {
-
-                if (i === 0 || grid[i - 1][j] === 0) {
+                if(i === 0 || grid[i-1][j] === 0) {
                     count++;
                 }
-                if (i === grid.length - 1 || grid[i + 1][j] === 0) {
+                if(i === grid.length-1 || grid[i+1][j] === 0) {
                     count++;
                 }
-                if (j === 0 || grid[i][j - 1] === 0) {
+                if(j === 0 || grid[i][j-1] === 0) {
                     count++;
                 }
-                if (j === grid[i].length - 1 || grid[i][j + 1] === 0) {
+                if(j === grid[i].length-1 || grid[i][j+1] === 0) {
                     count++;
                 }
             }
--- 16.10/matrix.js
+++ 16.10/matrix.js
@@ -6,18 +6,18 @@ let matrix = [
 ['0', '0', '0', 'M', '0'],
 ];
 let size = matrix.length;
-for(let i=0;i<size;i++){
-    for(let j=0;j<size;j++){
-        if(matrix[i][j]==='0'){
-            let count=0
-        for(let x=i-1;x<=i+1;x++){
-            for(let y=j-1;y<=j+1;y++){
-                if(x>=0 && x<size && y>=0 && y<size && matrix[x][y]==='M'){
+for(let i = 0; i < size; i++){
+    for(let j = 0; j < size; j++){  
+        if(matrix[i][j] === '0'){
+            let count = 0
+        for(let x = i-1 ;x <= i+1; x++){
+            for(let y = j-1; y <= j+1; y++){
+                if(x >= 0 && x < size && y >= 0 && y < size && matrix[x][y] === 'M'){
                     count++
                 }
             }
         }
-        matrix[i][j]=count.toString();
+        matrix[i][j] = count.toString();
     }
     }
 }
